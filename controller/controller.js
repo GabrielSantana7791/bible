1bd1ca0bd8e79ceadbac81fccde3dba2c41c6027
GabrielSantana7791 <89788334+GabrielSantana7791@users.noreply.github.com>
2022-11-10 20:22:09 -0300

    add redirect to https (etc/others/ssl/<files>)

--- controller/controller.js
+++ controller/controller.js
@@ -13,43 +13,78 @@ export function run(app) {
     const __dirname = path.dirname(__filename);
 
     app.use(express.static(path.join(__dirname, '..', 'src', '/public')));
-    
-    app.get('/', async (req, res) => {
-        const indexModel = new IndexModel();
-        const index = await indexModel.run();
 
-        res.send(index);
+    app.get('/', async (req, res) => {
+        if (!req.secure) {
+            res.redirect("https://" + req.headers.host + req.url);
+
+        } else {
+            const indexModel = new IndexModel();
+            const index = await indexModel.run();
+
+            res.send(index);
+        }
+
     })
 
     app.get('/devocional', async (req, res) => {
-        const devocionalModel = new DevocionalModel();
-        const devocional = await devocionalModel.run();
+        if (!req.secure) {
+            res.redirect("https://" + req.headers.host + req.url);
 
-        res.send(devocional);
+        } else {
+            const devocionalModel = new DevocionalModel();
+            const devocional = await devocionalModel.run();
+
+            res.send(devocional);
+        }
+
     })
 
     app.get('/bible', async (req, res) => {
-        const booksModel = new BooksModel();
-        const books = await booksModel.run();
-        res.send(books);
+        if (!req.secure) {
+            res.redirect("https://" + req.headers.host + req.url);
+
+        } else {
+            const booksModel = new BooksModel();
+            const books = await booksModel.run();
+            res.send(books);
+        }
     })
 
     app.get('/bible/:abbrev', async (req, res) => {
-        const bookModel = new BookModel();
-        const book = await bookModel.run(req.params.abbrev);
-        res.send(book);
+        if (!req.secure) {
+            res.redirect("https://" + req.headers.host + req.url);
+
+        } else {
+            const bookModel = new BookModel();
+            const book = await bookModel.run(req.params.abbrev);
+            res.send(book);
+        }
+
     })
 
     app.get('/bible/:abbrev/:chapter/', async (req, res) => {
-        const versesModel = new VersesModel();
-       const book = await versesModel.run(req.params.abbrev, req.params.chapter);
-        res.send(book);
+        if (!req.secure) {
+            res.redirect("https://" + req.headers.host + req.url);
+
+        } else {
+            const versesModel = new VersesModel();
+            const book = await versesModel.run(req.params.abbrev, req.params.chapter);
+            res.send(book);
+        }
+
     })
 
     app.get('/bible/:book/:chapter/:number', async (req, res) => {
-        const verseModel = new VerseModel();
-        const verse = await verseModel.run(req.params.book, req.params.chapter,
-            req.params.number);
-        res.send(verse);
-    })   
+        if (!req.secure) {
+            res.redirect("https://" + req.headers.host + req.url);
+
+        } else {
+            const verseModel = new VerseModel();
+            const verse = await verseModel.run(req.params.book, req.params.chapter,
+                req.params.number);
+            res.send(verse);
+        }
+
+    })
 }
--- js.js
+++ js.js
@@ -2,9 +2,19 @@ import express from 'express'
 import {run} from './controller/controller.js'
 import dailyTimer  from './etc/classes/dailyTimer.js'
 import DailyVerse from './etc/classes/daily/dailyVerse.js';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
-app.listen(8080)
+app.listen(80);
+
+let httpsSever = https;
+
+httpsSever.createServer({
+    cert: fs.readFileSync("./etc/others/ssl/code.crt"),
+    key: fs.readFileSync("./etc/others/ssl/code.key")
+    
+}, app).listen(443);
 
 run(app);
 
